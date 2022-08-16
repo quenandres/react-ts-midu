ec6d19ed907f101af88be08831e0e61e2a538e49
Jhonatan Mejia <quenandres@gmail.com>
2022-08-16 12:51:37 -0500

    Comprendiendo errores de TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import React,{ useState } from 'react';
+import React,{ useEffect, useState } from 'react';
 import './App.css';
 
+const INITIAL_STATE = [{
+  nick: 'dapelu',
+  subMonths: 3,
+  avatar: 'https://i.pravatar.cc/150?u=dapelu',
+  description: 'Dapelu es moderador'
+},
+{
+  nick: 'sergio_serrado',
+  subMonths: 7,
+  avatar: 'https://i.pravatar.cc/150?u=sergio_serrano',      
+}];
+
+interface Sub {
+  nick: string,
+  avatar: string,
+  subMonths: number,
+  description?: string
+}
+
 function App() {
-  const [subs, setSubs] = useState([
-    {
-      nick: 'dapelu',
-      subMonths: 3,
-      avatar: 'https://i.pravatar.cc/150?u=dapelu',
-      description: 'Dapelu es moderador'
-    },
-    {
-      nick: 'sergio_serrado',
-      subMonths: 7,
-      avatar: 'https://i.pravatar.cc/150?u=sergio_serrano',      
-    }
+  const [subs, setSubs] = useState<Array<Sub>>([
+    
   ]);
+
+  useEffect(() => {
+    setSubs(INITIAL_STATE);
+  }, [])
+  
 
 
   return (
