27f5e4c8a4d0eca84324d6eac579809073dfe66a
Jhonatan Mejia <quenandres@gmail.com>
2022-08-19 15:02:13 -0500

    UseReducer con TS

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useReducer} from 'react';
 import {Sub} from './../types';
 
 interface FormState {
@@ -9,24 +9,68 @@ interface FormProps {
     onNewSub: (newSub: Sub) => void
 }
 
+const INITIAL_STATE = {
+  nick: '',
+  subMonths: 0,
+  avatar: '',
+  description: ''
+}
+
+type FormReducerAction = {
+  type: "change_value",
+  payload: {
+    inputName: string,
+    inputValue: string
+  }
+} | {
+  type: "clear"
+}
+
+const formReducer = (state: FormState["inputValues"], action: FormReducerAction) => {
+  switch(action.type) {
+    case "change_value":
+      const {inputName, inputValue} = action.payload;
+      return {
+        ...state,
+        [inputName]: inputValue
+      }
+    case "clear":
+      return INITIAL_STATE
+  }
+}
+
 export const Form = ({onNewSub}: FormProps) => {
-    const [inputValues, setInputValues] = useState<FormState["inputValues"]>({
-        nick: '',
-        subMonths: 0,
-        avatar: '',
-        description: ''
-    })
+  //const [inputValues, setInputValues] = useState<FormState["inputValues"]>(INITIAL_STATE)
+  const [inputValues, dispatch] = useReducer(formReducer, INITIAL_STATE);
     
   const handleSubmit = (evt:React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-    onNewSub(inputValues);
+    onNewSub(inputValues);    
+    handleClear();
   };
 
-  const handleChange = (e:React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {    
-    setInputValues({
+  
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {   
+    const {name, value} = e.target;
+    dispatch({
+      type: 'change_value',
+      payload: {
+        inputName: name,
+        inputValue: value
+      }
+    });
+    /*setInputValues({
         ...inputValues,
         [e.target.name]: e.target.value
+    });*/
+  }
+
+  const handleClear = () => {
+    dispatch({
+      type: 'clear'
     })
+    //setInputValues(INITIAL_STATE)
   }
 
   return (
@@ -35,6 +79,8 @@ export const Form = ({onNewSub}: FormProps) => {
         <input onChange={handleChange} value={inputValues.subMonths} type="number" name="subMonths" placeholder="subMonths" />
         <input onChange={handleChange} value={inputValues.avatar} type="text" name="avatar" placeholder="avatar" />
         <textarea onChange={handleChange} value={inputValues.description} name="description" placeholder="description" ></textarea>
+        
+        <button type="button" onClick={handleClear}>Reset</button>
         <button type="submit">Save new sub</button>
     </form>
   )
