48bd53f95b592f0af8416a9edff63e85a448c92c
Jhonatan Mejia <quenandres@gmail.com>
2022-08-19 16:07:37 -0500

    UseReducer como custom Hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,47 +1,15 @@
 import {useState, useReducer} from 'react';
 import {Sub} from './../types';
+import useNewFormSub from '../hooks/useNewSubForm';
 
-interface FormState {
-    inputValues: Sub
-}
 
 interface FormProps {
     onNewSub: (newSub: Sub) => void
 }
 
-const INITIAL_STATE = {
-  nick: '',
-  subMonths: 0,
-  avatar: '',
-  description: ''
-}
-
-type FormReducerAction = {
-  type: "change_value",
-  payload: {
-    inputName: string,
-    inputValue: string
-  }
-} | {
-  type: "clear"
-}
-
-const formReducer = (state: FormState["inputValues"], action: FormReducerAction) => {
-  switch(action.type) {
-    case "change_value":
-      const {inputName, inputValue} = action.payload;
-      return {
-        ...state,
-        [inputName]: inputValue
-      }
-    case "clear":
-      return INITIAL_STATE
-  }
-}
-
 export const Form = ({onNewSub}: FormProps) => {
   //const [inputValues, setInputValues] = useState<FormState["inputValues"]>(INITIAL_STATE)
-  const [inputValues, dispatch] = useReducer(formReducer, INITIAL_STATE);
+  const [inputValues, dispatch] = useNewFormSub();
     
   const handleSubmit = (evt:React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
